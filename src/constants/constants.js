dfa2bcffa5974f4b8e3e760cbab69e25d7bfe6b9
Chandan Kumar <cksharma122@gmail.com>
2019-11-07 15:01:24 +0530

    Refactor labels and messages

---
+++ src/constants/constants.js
@@ -0,0 +1,13 @@
+const TABLES = {
+  todos: "todos",
+  todoUsers: "todoUsers"
+};
+
+const LABLES = {
+  LOGIN_ERROR: "username or password is incorrect"
+};
+
+module.exports = {
+  TABLES,
+  LABLES
+};
--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -4,31 +4,33 @@ const {
   executeSignUpQuery
 } = require("../utils/dbUtils");
 
+const { TABLES, LABLES } = require("../constants/constants");
+
 const signUpHandler = async (req, res) => {
   const { username, password } = req.body;
   const signUpStatus = await executeSignUpQuery(username);
   if (signUpStatus.error) {
     return res.send(JSON.stringify(signUpStatus.message));
   }
-  await insertQuery("todoUsers", { username, password });
-  res.redirect("/home");
+  await insertQuery(TABLES.todoUsers, { username, password });
+  res.redirect("/login");
 };
 
 const loginHandler = async (req, res) => {
   const { username, password } = req.body;
-  const query = `select * from todoUsers where todoUsers.username="${username}" AND todoUsers.password="${password}";`;
+  const query = `select * from ${TABLES.todoUsers} where ${TABLES.todoUsers}.username="${username}" AND ${TABLES.todoUsers}.password="${password}";`;
   const result = await executeQuery(query);
   if (result.length) {
     res.cookie("username", username);
     return res.redirect("/home");
   }
-  return res.send("username or password is incorrect");
+  return res.send(LABLES.LOGIN_ERROR);
 };
 
 const addTodoHandler = async (req, res) => {
   const { todo } = req.body;
   const { username } = req.cookies;
-  const result = await insertQuery("todos", {
+  const result = await insertQuery(TABLES.todos, {
     todo,
     username,
     time: JSON.stringify(Date.now()),
@@ -39,7 +41,7 @@ const addTodoHandler = async (req, res) => {
 
 const getTodoHandler = async (req, res) => {
   const { username } = req.cookies;
-  const query = `select * from todos where username="${username}"`;
+  const query = `select * from ${TABLES.todos} where username="${username}"`;
   const todos = await executeQuery(query);
   res.send(JSON.stringify(todos));
 };
