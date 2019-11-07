f55eb311dd3c36277b94bdb6c2f6ce08ff3b7466
Chandan Kumar <cksharma122@gmail.com>
2019-11-07 16:55:29 +0530

    Refactor: Get to a stable stage - Remove all console log

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,9 +2,6 @@ import React from "react";
 import Todo from "../Todo/Todo";
 
 const TodoList = ({ todos }) => {
-  console.log(todos);
-
-  console.log("die");
   return (
     <div>
       {todos.map(todo => {
